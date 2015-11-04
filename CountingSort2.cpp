3a94ea812dbbd7a0882af6605012e9f08471122e
allaudeen <allaudeen.n@gmail.com>
2015-11-04 14:59:31 +0530

    Create CountingSort2.cpp

---
+++ CountingSort2.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    
+    map<int,int> ar;
+    int nInput;
+    for(int i = 0; i < n; i++) {
+        cin>>nInput;
+        
+        if(ar.size() == 0 || ar.find(nInput) == ar.end()) 
+            ar.insert(pair<int,int>(nInput,1));
+        else if(ar.find(nInput) != ar.end())
+            ar.insert(pair<int,int>(nInput,(ar.find(nInput)->second)++));
+    }
+    int nOccurence;
+    for(int i = 0; i < 100; i++) {
+        if(ar.find(i) != ar.end()) {
+            nOccurence = ar.find(i)->second;
+            for(int j = 0; j < nOccurence; j++){
+                cout<<i<<" ";
+            }
+        }
+
+    }
+    return 0;
+}
